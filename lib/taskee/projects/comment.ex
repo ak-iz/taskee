baf5388c1a9b044bf6604c0e571e64dd9608ec87
ak-iz <anas.khalid@invozone.com>
2023-06-05 19:14:51 +0500

    comments schema add datetime

--- lib/taskee/projects/comment.ex
+++ lib/taskee/projects/comment.ex
@@ -4,6 +4,7 @@ defmodule Taskee.Projects.Comment do
 
   schema "comments" do
     field :text, :string
+    field :datetime, :utc_datetime
     belongs_to :user, Taskee.Accounts.User
     belongs_to :task, Taskee.Projects.Task
 
@@ -13,7 +14,7 @@ defmodule Taskee.Projects.Comment do
   @doc false
   def changeset(comment, attrs) do
     comment
-    |> cast(attrs, [:text])
-    |> validate_required([:text])
+    |> cast(attrs, [:text, :datetime, :user_id, :task_id])
+    |> validate_required([:text, :datetime, :user_id, :task_id])
   end
 end
