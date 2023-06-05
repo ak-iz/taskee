fe13a85a62cb5ad12fdb35620518f478838362c9
ak-iz <anas.khalid@invozone.com>
2023-06-05 17:16:08 +0500

    edit first migrations

--- priv/repo/migrations/20230605120944_create_comments.exs
+++ priv/repo/migrations/20230605120944_create_comments.exs
@@ -4,7 +4,7 @@ defmodule Taskee.Repo.Migrations.CreateComments do
   def change do
     create table(:comments) do
       add :text, :string
-      add :user, references(:user, on_delete: :delete_all)
+      add :user, references(:users, on_delete: :delete_all)
       add :task, references(:tasks, on_delete: :delete_all)
 
       timestamps()
--- priv/repo/migrations/20230605121042_create_members.exs
+++ priv/repo/migrations/20230605121042_create_members.exs
@@ -4,7 +4,7 @@ defmodule Taskee.Repo.Migrations.CreateMembers do
   def change do
     create table(:members) do
       add :role_id, :integer
-      add :user, references(:user, on_delete: :delete_all)
+      add :user, references(:users, on_delete: :delete_all)
       add :project, references(:projects, on_delete: :delete_all)
 
       timestamps()
